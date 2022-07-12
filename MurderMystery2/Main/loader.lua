517b8a3fa543567789d79bb66bfa8daa9b2f9b0c
clevs <96828887+clevsgm@users.noreply.github.com>
2022-07-12 03:54:39 -0500

    Update loader.lua

--- MurderMystery2/Main/loader.lua
+++ MurderMystery2/Main/loader.lua
@@ -1,34 +1,28 @@
+-- [[ Global ]]
+getgenv().bSpamTrap = false
+getgenv().toggleInfGhost = false
+
 --[[ Destroy() ]]
 if game:GetService("CoreGui"):FindFirstChild("Harry Potter") then
     game:GetService("CoreGui")["Harry Potter"]:Destroy()
     wait(0.3)
 end
 --[[ Library and Gui ]]
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/IreXion-UI-Library/main/Revamp/IreXion%20UI%20Library"))()
+local sss = "https://raw.githubusercontent.com/RegularVynixu/IreXion-UI-Library/main/Revamp/IreXion%20UI%20Library"
+local Library = loadstring(game:HttpGet(sss))()
 local Gui = Library:AddGui({
     Title = {"Harry", "Potter"},
     ThemeColor = Color3.fromRGB(255, 0, 0),
     ToggleKey = Enum.KeyCode.RightControl,
 })
--- Rainbow --
-game.RunService.Stepped:Connect(function()
+game.RunService.Stepped:Connect(function()--Rainbow
     Gui:SetTheme(Color3.fromHSV(tick() % 5 / 5, 1, 1))
 end)
-
-
--- [[ Global ]]
-getgenv().bSpamTrap = false
-getgenv().toggleInfGhost = false
+--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//--//--
 --[[ Home Tab ]]
 local homeTab = Gui:AddTab("Home")
---[[ Categories ]]
-local EspCategory = homeTab:AddCategory("Esp")
-local EffectsCategory = homeTab:AddCategory("Perks")
-local TpCategory = homeTab:AddCategory("Teleport")
-local EmotesCategory = homeTab:AddCategory("Emotes")
-local AutofarmCategory = homeTab:AddCategory("Autofarm")
-
 --[[ ESP ]]
+local EspCategory = homeTab:AddCategory("Visuals")
 local AEsp = EspCategory:AddToggle("All Esp", false, function(toggle)
     print(toggle)
 end)
@@ -38,8 +32,8 @@ end)
 local SEsp = EspCategory:AddToggle("Sheriff Esp", false, function(toggle)
     findSheriff()
 end)
-
 --[[ Effects ]]
+local EffectsCategory = homeTab:AddCategory("Perks")
 local tInfGhost = EffectsCategory:AddToggle("Infinite Ghost", false, function(bool)
     getgenv().toggleInfGhost = bool
     infiniteGhost(bool)
@@ -55,11 +49,14 @@ local spamTrapTog = EffectsCategory:AddToggle("Spam Traps", false, function(bool
     getgenv().bSpamTrap = bool
     betterSpamTrap(bool, time) 
 end)
+local Button = EffectsCategory:AddButton("Trap Murderer", function()
+    trapMurderer()
+end)
 local Button = EffectsCategory:AddButton("Free Fake Gun", function()
     freeFakeGun()
 end)
-
 --[[ Teleport ]]
+local TpCategory = homeTab:AddCategory("Teleport")
 local Button = TpCategory:AddButton("Lobby", function()
     local yomero = game:GetService("Players").LocalPlayer
     yomero.Character.Humanoid.RootPart.CFrame = CFrame.new(-107.80003356933594, 138.3238525390625, 34.399967193603516)
@@ -74,8 +71,8 @@ end)
 local Box = TpCategory:AddBox("Teleport to Player", function(text)
     local mmg = text
 end)
-
 --[[ Emotes ]]
+local EmotesCategory = homeTab:AddCategory("Emotes")
 local Button = EmotesCategory:AddButton("Sit", function()
     emotiza("sit")
 end)
@@ -97,16 +94,14 @@ end)
 local Button = EmotesCategory:AddButton("Headless", function()
     emotiza("headless")
 end)
-
+--[[ Autofarm ]]
+local AutofarmCategory = homeTab:AddCategory("Autofarm")
 
 
 --[[ Universal ]]
 local uvTab = Gui:AddTab("Universal")
---[[ Categories ]]
-local PlayerCategory = uvTab:AddCategory("Player")
-local CameraCategory = uvTab:AddCategory("Camera")
-
 --[[ Player ]]
+local PlayerCategory = uvTab:AddCategory("Player")
 local sWalkSpeed = PlayerCategory:AddSlider("Walk Speed", 1, 100, 16, true, function(value)
     local yom = game:GetService("Players").LocalPlayer
     yom.Character.Humanoid.WalkSpeed = value
@@ -123,8 +118,8 @@ local Button = PlayerCategory:AddButton("Default", function()
     sJumpPower:Set(50)
     sJumpPower:Set(50)
 end)
-
 --[[ Camera ]]
+local CameraCategory = uvTab:AddCategory("Camera")
 local sFOV = CameraCategory:AddSlider("FOV", 10, 120, 70, true, function(value)
     local cam = game:GetService("Workspace").Camera
     cam.FieldOfView = value
@@ -134,28 +129,22 @@ local Button = CameraCategory:AddButton("Default", function()
     sFOV:Set(70)
 end)
 
-
-
 --[[ Settings ]]
 local SettingsTab = Gui:AddTab("Settings")
---[[ Categories ]]
-local ServerCategory = SettingsTab:AddCategory("Server")
-local KeyBindsCategory = SettingsTab:AddCategory("KeyBinds")
-local DestroyCategory = SettingsTab:AddCategory("Destroy")
-
 --[[ Server ]]
+local ServerCategory = SettingsTab:AddCategory("Server")
 local bRj = ServerCategory:AddButton("Rejoin", function()
     local tps = game:GetService("TeleportService")
     local yom = game:GetService("Players").LocalPlayer
     tps:TeleportToPlaceInstance(game.PlaceId,game.JobId,tps)
 end)
-
 local bHopServ = ServerCategory:AddButton("Hop Server", function()
     local tpservice= game:GetService("TeleportService")
     local plr = game.Players.LocalPlayer
     tpservice:Teleport(game.PlaceId, plr)
 end)
 --[[ Key Binds ]]
+local KeyBindsCategory = SettingsTab:AddCategory("KeyBinds")
 local ToggleBind = KeyBindsCategory:AddToggleBind("Trap's Placer", Enum.KeyCode.R, false, function()
     betterPlaceTrap()
 end)
@@ -171,11 +160,10 @@ local ToggleBind = KeyBindsCategory:AddToggleBind("Zen", Enum.KeyCode.Z, false, 
     emotiza("zen")
 end)
 --[[ Destroy ]]
+local DestroyCategory = SettingsTab:AddCategory("Destroy")
 local Button = DestroyCategory:AddButton("Destroy", function()
     game:GetService("CoreGui")["Harry Potter"]:Destroy()
 end)
 
-
-
--- Load Functions
+-- [[ Load Functions ]] --
 loadstring(game:HttpGet("https://raw.githubusercontent.com/clevsgm/clevs/main/MurderMystery2/Require/1oader.lua"))()
