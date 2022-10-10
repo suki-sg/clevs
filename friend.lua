8f42888851fe6aae65054c7b53ac3d90fade9f14
Levy <96828887+levy-gm@users.noreply.github.com>
2022-10-10 09:31:53 -0700

    Create friend.lua

---
+++ friend.lua
@@ -0,0 +1,14 @@
+repeat task.wait() until game:IsLoaded()
+local CoreGui = game:GetService("StarterGui")
+local players = game:GetService("Players")
+
+players.PlayerAdded:Connect(function(player)
+	if player:IsFriendsWith(players.LocalPlayer.UserId) then
+		CoreGui:SetCore("SendNotification",{
+			Title = "Player Joined",
+			Text = player.Name.. " has joined your game",
+			Icon = "rbxasset://textures/ui/icon_friends_16.png",
+			Duration = 5
+		})
+	end
+end)
